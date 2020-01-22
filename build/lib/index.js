b82d63a0f1b045819b5a47079bd06bb15263736a
ebayboy <ebayboy@163.com>
2020-01-22 12:39:25 +0800

    create base line.

---
+++ build/lib/index.js
@@ -0,0 +1,4 @@
+"use strict";
+
+console.log("hello world");
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIi4uLy4uL2xpYi9pbmRleC5qcyJdLCJuYW1lcyI6WyJjb25zb2xlIiwibG9nIl0sIm1hcHBpbmdzIjoiOztBQUFBQSxRQUFRQyxHQUFSLENBQVksYUFBWiIsImZpbGUiOiJpbmRleC5qcyIsInNvdXJjZXNDb250ZW50IjpbImNvbnNvbGUubG9nKFwiaGVsbG8gd29ybGRcIik7Il19
