558b3423a6f304599fe843353056d22f7e0ace13
ebayboy <ebayboy@163.com>
2020-01-22 13:15:01 +0800

    fw

--- build/lib/index.js
+++ build/lib/index.js
@@ -1,4 +1,15 @@
 "use strict";
 
-console.log("hello world");
-//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIi4uLy4uL2xpYi9pbmRleC5qcyJdLCJuYW1lcyI6WyJjb25zb2xlIiwibG9nIl0sIm1hcHBpbmdzIjoiOztBQUFBQSxRQUFRQyxHQUFSLENBQVksYUFBWiIsImZpbGUiOiJpbmRleC5qcyIsInNvdXJjZXNDb250ZW50IjpbImNvbnNvbGUubG9nKFwiaGVsbG8gd29ybGRcIik7Il19
+//console.log("hello world");
+
+//1. babel lib -d build\lib
+//2. npm run build
+
+var a = 2;
+{
+    let a = 3;
+    console.log("in:", a);
+}
+
+console.log("out:", a);
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIi4uLy4uL2xpYi9pbmRleC5qcyJdLCJuYW1lcyI6WyJhIiwiY29uc29sZSIsImxvZyJdLCJtYXBwaW5ncyI6Ijs7QUFBQTs7QUFFQTtBQUNBOztBQUVBLElBQUlBLElBQUksQ0FBUjtBQUNBO0FBQ0ksUUFBSUEsSUFBSSxDQUFSO0FBQ0FDLFlBQVFDLEdBQVIsQ0FBWSxLQUFaLEVBQW1CRixDQUFuQjtBQUNIOztBQUVEQyxRQUFRQyxHQUFSLENBQVksTUFBWixFQUFvQkYsQ0FBcEIiLCJmaWxlIjoiaW5kZXguanMiLCJzb3VyY2VzQ29udGVudCI6WyIvL2NvbnNvbGUubG9nKFwiaGVsbG8gd29ybGRcIik7XHJcblxyXG4vLzEuIGJhYmVsIGxpYiAtZCBidWlsZFxcbGliXHJcbi8vMi4gbnBtIHJ1biBidWlsZFxyXG5cclxudmFyIGEgPSAyO1xyXG57XHJcbiAgICBsZXQgYSA9IDM7XHJcbiAgICBjb25zb2xlLmxvZyhcImluOlwiLCBhKTtcclxufVxyXG5cclxuY29uc29sZS5sb2coXCJvdXQ6XCIsIGEpOyJdfQ==
